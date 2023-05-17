8323ad1d16918d255aa9274de405366fe69e6690
Sean Sapi <sapitanansean@gmail.com>
2023-05-17 12:35:32 +0400

    slight update

--- Crocycle/WebFiles/auth.php
+++ Crocycle/WebFiles/auth.php
@@ -1,12 +1,8 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'crocycledb';
-// Try and connect using the info above.
-$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+// Try and connect using this info
+$conn = mysqli_connect("localhost", "root", "", "crocycledb");
+
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
