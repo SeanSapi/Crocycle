2d61f80a94b338810a3d89acfab3a2d5a42049f9
Sean Sapi <sapitanansean@gmail.com>
2023-05-18 09:51:42 +0400

    minor auth.php update

--- Crocycle/WebFiles/auth.php
+++ Crocycle/WebFiles/auth.php
@@ -9,7 +9,7 @@ if ( mysqli_connect_errno() ) {
 }
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['username'], $_POST['password']) ) {
+if (!isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
