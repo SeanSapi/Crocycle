6312e0cf18ad780a0cb3b45fa6054d7c4c2a381e
BathspaJohnAndreFederiso <71628636+BathspaJohnAndreFederiso@users.noreply.github.com>
2023-05-23 16:13:07 +0400

    5/23/2023 Commit
    - Changed some  media queries
    - Developed the cart some more

--- Crocycle/WebFiles/PurePHP/ProductDB.php
+++ Crocycle/WebFiles/PurePHP/ProductDB.php
@@ -1,10 +1,9 @@
 <?php
-session_start();
 
-$conn = mysqli_connect("localhost", "root", "", "crocycledb2"); // change the last parameter in this to whatever your database's name is 
+include "../connection.php";
 
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+function template_header($title){
+	
 }
+
 ?>
--- Crocycle/WebFiles/PurePHP/ShopList.php
+++ Crocycle/WebFiles/PurePHP/ShopList.php
@@ -1,3 +1,11 @@
 <?php
 
+include "../connection.php";
+
+session_start();
+
+if (isset($_POST[""])) {
+    $product_id = ('product');
+}
+
 ?>
